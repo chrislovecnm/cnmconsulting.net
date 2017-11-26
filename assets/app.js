304aa8e1ed6c293c64060ae50572ad478d7604af
chrislovecnm <clove@cnmconsulting.net>
2017-11-25 17:10:04 -0700

    scroll is working

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,7 @@
 ---
 ---
+
+{% include_relative _js/jquery.js %}
 {% include_relative _js/bootstrap.min.js %}
+{% include_relative _js/jquery.easing.min.js %}
+{% include_relative _js/scrolling-nav.js %}
